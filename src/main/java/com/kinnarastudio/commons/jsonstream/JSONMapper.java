0cdae167804fe171a92519e391d2cbd832c96882
aristosh <aristo.hadisoeganda@gmail.com>
2021-04-23 20:41:49 +0700

    1.2.5

---
+++ src/main/java/com/kinnarastudio/commons/jsonstream/JSONMapper.java
@@ -0,0 +1,23 @@
+package com.kinnarastudio.commons.jsonstream;
+
+import com.kinnarastudio.commons.Try;
+import org.json.JSONObject;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+public interface JSONMapper {
+    static <T, V> Function<T, JSONObject> toJSONObject(Function<T, String> keyExtractor, Function<T, V> valueExtractor) {
+        return Try.onFunction((t) -> {
+            Objects.requireNonNull(keyExtractor);
+            Objects.requireNonNull(valueExtractor);
+            
+            String key = keyExtractor.apply(t);
+            V value = valueExtractor.apply(t);
+
+            JSONObject jsonObject =  new JSONObject();
+            jsonObject.put(key, value);
+            return jsonObject;
+        });
+    }
+}
