d78cb3c84dda1cccd7022988ee98562cc24f7c26
aristosh <aristo.hadisoeganda@gmail.com>
2021-10-28 12:47:11 +0700

    add json concat and json combine

--- src/main/java/com/kinnarastudio/commons/jsonstream/JSONMapper.java
+++ src/main/java/com/kinnarastudio/commons/jsonstream/JSONMapper.java
@@ -1,10 +1,12 @@
 package com.kinnarastudio.commons.jsonstream;
 
 import com.kinnarastudio.commons.Try;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public final class JSONMapper {
     public static <T, V> Function<T, JSONObject> toJSONObject(Function<T, String> keyExtractor, Function<T, V> valueExtractor) {
@@ -19,5 +21,16 @@ public final class JSONMapper {
             jsonObject.put(key, value);
             return jsonObject;
         });
+    }
+
+    public static JSONArray concat(JSONArray array1, JSONArray array2) {
+        return Stream
+                .concat(JSONStream.of(array1, Try.onBiFunction(JSONArray::get)), JSONStream.of(array2, Try.onBiFunction(JSONArray::get)))
+                .collect(JSONCollectors.toJSONArray());
+    }
+
+    public static JSONObject combine(JSONObject object1, JSONObject object2) {
+        return Stream.concat(JSONStream.of(object1, Try.onBiFunction(JSONObject::get)), JSONStream.of(object2, Try.onBiFunction(JSONObject::get)))
+                .collect(JSONCollectors.toJSONObject(JSONObjectEntry::getKey, JSONObjectEntry::getValue));
     }
 }
